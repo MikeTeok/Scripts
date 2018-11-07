49fab59f14cf2d633d7d0f56ec4748f97abc910b
MikeTeok <miketeok94@gmail.com>
2018-11-07 21:54:49 +0800

    added chcp 65001
    chcp 65001 allowed chinese character in the script

--- dot_and_only_take_third_name.bat
+++ dot_and_only_take_third_name.bat
@@ -3,6 +3,7 @@ FOR /R %%f IN (*.*) DO CALL :UseLast "%%~f" "%%~nf"
 GOTO :EOF
 
 :UseLast
+chcp 65001
 FOR /F "TOKENS=1,3 DELIMS=." %%m IN (%2) DO IF "%%n"=="" (
     IF NOT "%~2"=="%~n1" REN %1 "%~2%~x1"
 ) ELSE CALL :UseLast %1 "%%n"
